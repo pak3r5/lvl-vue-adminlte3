904dc9ce1665a6fce2020c59452c06081d37bcae
unknown <pak3r5@gmail.com>
2020-02-21 11:36:23 -0600

    [add] first commit

---
+++ resources/js/app.js
@@ -0,0 +1,59 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ /
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ /
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ /
+
+const app = new Vue({
+    el: '#app',
+});
+*/
+
+
+require('./bootstrap');
+
+// 1. Comment out this following line:
+// window.Vue = require('vue');
+
+// 2. Add below the above commented-out line:
+import Vue from 'vue';
+import VueRouter from "vue-router";
+
+window.Vue = Vue;
+Vue.use(VueRouter);
+
+import Form from "./utilities/Form";
+window.Form = Form;
+
+import router from './routes';
+
+/* ... SNIP ... */
+
+// 3. Update the new Vue intance at the bottom of the file.
+new Vue({
+    el: '#app',
+    router
+});
